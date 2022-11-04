5ed5e99f924973a18dad4edc898c95412e94922b
Tony Bowden <tony@tmtm.com>
2022-11-04 10:48:35 +0200

    move results to subdir

--- holders.js
+++ holders.js
@@ -1,7 +1,8 @@
 module.exports = () => {
-  return `SELECT DISTINCT ?person ?start ?end WHERE {
+  return `SELECT DISTINCT ?person ?start ?end ?lastmod WHERE {
     ?person wdt:P31 wd:Q5 ; p:P39 ?ps .
     ?ps ps:P39 wd:Q19305384 ; pq:P580 ?start ; pq:P582 ?end .
+    ?person schema:dateModified ?lastmod .
   }
   # ${new Date().toISOString()}
   ORDER BY ?person ?start ?end ?ps`
